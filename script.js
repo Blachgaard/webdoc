e18a1aa19bdf3d000478e34ed6886c5e9b7e90a9
Blachgaard <36067636+Blachgaard@users.noreply.github.com>
2018-09-05 11:33:02 +0200

    måske fixed

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // stemningsvideo
-let NewVid = function(movie) {
+let newvid = function(movie) {
       // create a <video>
       let vid = document.createElement('video');
       vid.src = movie;
@@ -13,4 +13,4 @@ let NewVid = function(movie) {
     }
     let stemningsvideo = 'video/Stemnings_Video.mp4';
     // use the object
-    NewVid( stemningsvideo );
+    newvid( stemningsvideo );
