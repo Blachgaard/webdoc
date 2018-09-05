f5c87f50bd816a81a5f5dc251a769567d12fc715
Rasmus Haahr <eaarhpe@students.eaaa.dk>
2018-09-05 11:36:25 +0200

    sf

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // stemningsvideo
-let newvid = function(movie) {
+let NewVid = function(movie) {
       // create a <video>
       let vid = document.createElement('video');
       vid.src = movie;
@@ -13,8 +13,5 @@ let newvid = function(movie) {
     }
     let stemningsvideo = 'video/Stemnings_Video.mp4';
     // use the object
-<<<<<<< HEAD
     NewVid (stemningsvideo);
-=======
-    newvid( stemningsvideo );
->>>>>>> e18a1aa19bdf3d000478e34ed6886c5e9b7e90a9
+
