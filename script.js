0b58eaaa58c7f7d91cd2ad29a2c3d527136eabe5
Blachgaard <36067636+Blachgaard@users.noreply.github.com>
2018-09-05 12:10:05 +0200

    12

--- script.js
+++ script.js
@@ -3,15 +3,12 @@ let NewVid = function(movie) {
       // create a <video>
       let vid = document.createElement('video');
       vid.src = movie;
-      vid.style.width = '100%';
-      vid.style.maxWidth = '100%';
       vid.autoplay = true;
       vid.loop = true;
       vid.load();
       // append to <div id="stemvid">
       stemvid.appendChild(vid);
     }
-    let stemningsvideo = 'video/Stemnings_Video.mp4';
+    let stemningsvideo = 'video/stemningsvideo.mp4';
     // use the object
     NewVid( stemningsvideo );
-
